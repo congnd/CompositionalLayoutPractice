5bb7794d9bc00a6b73ca8c2eed1e07bc9103380d
congnd <congnd@outlook.com>
2019-11-18 23:28:52 +0900

    Initial Commit

---
+++ TestCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCollectionView
+//
+//  Created by Nguyen Dinh Cong on 2019/11/18.
+//  Copyright © 2019 Cong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
